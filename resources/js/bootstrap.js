0266e44cad88a87249a013948a42b73847c02ef2
philliprearick <psrearick@gmail.com>
2021-01-31 05:15:29 -0500

    update site baseURL for axios

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -8,7 +8,7 @@ window.axios = require('axios');
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
-window.axios.defaults.baseURL = 'https://kanban.prwebconsulting.com';
+window.axios.defaults.baseURL = 'https://kanbanext.prwebconsulting.com';
 
 window.axios.defaults.headers.common = {
     'X-Requested-With': 'XMLHttpRequest',
